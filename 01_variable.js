6220b268cd7e3f5b593828491d0a906fcaa1ed55
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-11-16 15:34:19 +0530

    first commit

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const accountId =1
+let accountEmail = "vaibhav@gmsil.com"
+var accountPassword ="1432555"
+accountCity = "sangamner"
+
+let accountState;
+
+
+
+
+accountEmail = "vaibhav@maz.com"
+accountPassword = "2322111"
+accountCity = "pune"
+
+console.log(accountId)
