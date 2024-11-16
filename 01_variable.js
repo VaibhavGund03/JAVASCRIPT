75511c0ad435dc533114fae6c1b9bbbe67ba56d6
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-11-16 15:47:06 +0530

    comment

--- 01_variable.js
+++ 01_variable.js
@@ -3,13 +3,22 @@ let accountEmail = "vaibhav@gmsil.com"
 var accountPassword ="1432555"
 accountCity = "sangamner"
 
-let accountState;
+let accountState ="maharashtra"
+ // this is mt they gives output is undefine.
 
 
+
+ 
+// following concept chnage the value oterwise before value are print
+/*
 
 
 accountEmail = "vaibhav@maz.com"
 accountPassword = "2322111"
 accountCity = "pune"
 
+
+*/
 console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
