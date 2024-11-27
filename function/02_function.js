fea81820f568f2a3f1983f6129ac2f0fba112ac3
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-11-27 19:24:21 +0530

    27-11-2024

---
+++ function/02_function.js
@@ -0,0 +1,40 @@
+// 1. use math round in js 
+function onePlusAveg(x,y){
+
+    console.log("Done....");
+
+    return 1+(x,y)/2
+    }
+    let a = 1;
+    let b = 2;
+    let c = 3;
+    
+    console.log("average of a and b is ",onePlusAveg(a,b));
+    console.log("average of a and c is ",onePlusAveg(a,c));
+    console.log("average of b and cis ",onePlusAveg(b,c));
+
+    console.log();
+
+    // 2. function create without parameters
+
+    const hello = ()=>{
+        
+        console.log("hey bro perfectly runn my program yaah.....");
+        console.log();
+
+    }
+    hello();
+             // 3. return a any value
+
+             const value =()=>{
+
+                console.log("...hey vaibhav...")
+                return("congratulation");
+
+             }
+             let v = value();
+             console.log(v);
+             
+ // in this page  me write a two way function one is using parameters and second is a without parameters
+ 
+ 
