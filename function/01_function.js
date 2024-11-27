91572d928f28f7b31b83608ac262566f7b7e69e6
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-11-27 18:54:07 +0530

    27-11-2024

---
+++ function/01_function.js
@@ -0,0 +1,18 @@
+// function to print the average and done.
+
+function onePlusAvg(x,y){
+
+   
+    return 1+(x+y)/2
+
+    console.log("Done.....")
+}
+let a = 1;
+let b = 2;
+let c = 3; 
+console.log("Done.....")
+console.log("the average of a  and b is ",onePlusAvg(a,b));
+console.log("the average of a  and c is ",onePlusAvg(a,c));
+console.log("the average of b  and c is ",onePlusAvg(b,c));
+
+
